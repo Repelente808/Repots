724ce232c8a1db7f8d5238365f60714a170ed4e3
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-18 12:10:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -154,7 +154,7 @@ cp -rf home/* home/.[!.]* home/..?* "$HOME/"
 
 sudo modprobe i2c-dev
 sudo usermod -aG i2c $USER
-
+gsettings set org.gnome.desktop.interface gtk-theme 'catpuccin-mocha-peach-standart+default'
 echo "Instalation done!"
 
 echo "Now the system will return to your login manager, just login again!"
