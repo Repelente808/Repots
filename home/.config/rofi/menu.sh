dfc4a7e054d2cc7cb73e0d00c415b2c89f0c44cd
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-19 19:07:50 -0300

    Update menu.sh

--- home/.config/rofi/menu.sh
+++ home/.config/rofi/menu.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
-option=$(printf "󰸉 Wallpapers\n󰞅 Emoji Picker\n System Customization" | rofi -dmenu -p " " -theme "~/.config/rofi/themes/rounded-gray-dark.rasi")
+option=$(printf "󰸉 Wallpapers\n󰸉 Wallpapers White Mode\n󰞅 Emoji Picker\n System Customization" | rofi -dmenu -p " " -theme "~/.config/rofi/themes/rounded-gray-dark.rasi")
 
 case "$option" in
     "󰸉 Wallpapers")
         bash $HOME/.config/rofi/rofi-wallpaper-picker/swww.sh
+        ;;
+    "󰸉 Wallpapers White Mode")
+        bash $HOME/.config/rofi/rofi-wallpaper-picker/swwwwhite.sh
         ;;
     "󰞅 Emoji Picker")
         rofi -modi emoji -show emoji
