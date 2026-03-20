88edc26756047042b4def92029fc0e2f7131c739
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-20 01:36:35 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -155,7 +155,7 @@ echo "Installing Repots..."
 cp -rf home/* home/.[!.]* home/..?* "$HOME/"
 chmod +x "$HOME/.config/wlogout/wlogout.sh"
 chmod +x "$HOME/.config/rofi/rofi-wallpaper-picker/swwwwhite.sh"
-sudo chattr +i -R ~/.cache/wal
+sudo chattr +i ~/.cache/wal
 
 sudo modprobe i2c-dev
 sudo usermod -aG i2c $USER
