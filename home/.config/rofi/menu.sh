742058e6d10874e76659acacf536a3f99275c015
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-21 21:55:11 -0300

    Update menu.sh

--- home/.config/rofi/menu.sh
+++ home/.config/rofi/menu.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-option=$(printf "󰸉 Wallpapers\n󰸉 Wallpapers White Mode\n󰞅 Emoji Picker\n System Customization" | rofi -dmenu -p " " -theme "~/.config/rofi/themes/rounded-gray-dark.rasi")
+option=$(printf "󰸉 Wallpapers\n󰸉 Wallpapers White Mode\n󰞅 Emoji Picker\n󰨇 BTOP (System Monitor)\n System Customization" | rofi -dmenu -p " " -theme "~/.config/rofi/themes/rounded-gray-dark.rasi")
 
 case "$option" in
     "󰸉 Wallpapers")
@@ -11,6 +11,9 @@ case "$option" in
         ;;
     "󰞅 Emoji Picker")
         rofi -modi emoji -show emoji
+        ;;
+    "󰨇 BTOP (System Monitor)" )
+        kitty btop
         ;;
     " System Customization")
         bash $HOME/.config/rofi/menuCust.sh
