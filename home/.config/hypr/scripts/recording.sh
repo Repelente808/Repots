fd7719976d409188d24bf215095d362169722c33
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-22 22:38:48 -0300

    Update recording.sh

--- home/.config/hypr/scripts/recording.sh
+++ home/.config/hypr/scripts/recording.sh
@@ -3,7 +3,7 @@
 # Verifica se o wf-recorder já está rodando
 if pgrep -x "wf-recorder" > /dev/null; then
     pkill -SIGINT wf-recorder
-    notify-send "Recording Stopped" "Recording complete saved in folder /Vídeos/Rec_$."
+    notify-send "Recording Stopped" "Recording complete saved in folder /Vídeos/Rec."
 else
     wf-recorder --audio=$(pactl get-default-sink).monitor -f "$HOME/Vídeos/Rec/Rec_$(date +%d-%m-%Y_%H-%M-%S).mp4" &
     notify-send "Recording" "Starting Desktop Recording (no mic)..."
