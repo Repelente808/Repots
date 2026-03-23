78215412f3b53d63fb81731bfaa8e695ee12a13a
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-23 00:47:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -170,6 +170,7 @@ chmod +x "$HOME/.config/hypr/scripts/gamemode.sh"
 chmod +x "$HOME/.config/hypr/scripts/oldtv.sh"
 chmod +x "$HOME/.config/rofi/SystemLooks.sh"
 chmod +x "$HOME/.config/hypr/scripts/recording.sh"
+chmod +x "$HOME/.config/hypr/scripts/hyprlaxtog.sh"
 sudo chattr +i ~/.cache/wal
 
 sudo modprobe i2c-dev
