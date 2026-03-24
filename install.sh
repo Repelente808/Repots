1293f68abdc68470b6ec186a8774ee0973b3faa4
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-24 15:00:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -171,7 +171,6 @@ chmod +x "$HOME/.config/rofi/SystemLooks.sh"
 chmod +x "$HOME/.config/hypr/scripts/recording.sh"
 chmod +x "$HOME/.config/hypr/scripts/hyprlaxtog.sh"
 chmod +x "$HOME/.config/hypr/scripts/fontchange.sh"
-sudo chattr +i ~/.cache/wal
 
 sudo modprobe i2c-dev
 sudo usermod -aG i2c $USER
