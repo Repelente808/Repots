bd851d5d7b404b4e16eb779c01521243ce024e14
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-24 15:01:16 -0300

    Delete home/.cache/wal directory

--- home/.cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c6c7ca", "#1e212b" },
-	[SchemeSel] = { "#c6c7ca", "#9EA0A0" },
-	[SchemeOut] = { "#c6c7ca", "#F6F8FC" },
-};
--- home/.cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#1e212b";
-static const char* selfgcolor   = "#c6c7ca";
-static const char* normbgcolor  = "#D9CBA3";
-static const char* normfgcolor  = "#c6c7ca";
-static const char* urgbgcolor   = "#9EA0A0";
-static const char* urgfgcolor   = "#c6c7ca";
