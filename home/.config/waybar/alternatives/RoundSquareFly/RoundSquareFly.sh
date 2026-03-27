02f6697b3c91d3400a8aeb954efecdaff172391e
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-27 18:20:58 -0300

    Add files via upload

---
+++ home/.config/waybar/alternatives/RoundSquareFly/RoundSquareFly.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm "/home/repelente808/.config/waybar/style.css" "/home/repelente808/.config/waybar/config.jsonc"
+
+cp -f "/home/repelente808/.config/waybar/alternatives/RoundSquareFly/config.jsonc" "/home/repelente808/.config/waybar"
+cp -f "/home/repelente808/.config/waybar/alternatives/RoundSquareFly/style.css" "/home/repelente808/.config/waybar"
+
+pkill waybar
+sleep 0.2
+waybar &
+echo "Changed to flatRound"
