8ef7d07d289806930646cda782b26f778b080d14
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-27 17:52:54 -0300

    Add files via upload

---
+++ home/.config/waybar/alternatives/FlatSquare/FlatSquare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm "/home/repelente808/.config/waybar/style.css" "/home/repelente808/.config/waybar/config.jsonc"
+
+cp -f "/home/repelente808/.config/waybar/alternatives/FlatSquare/config.jsonc" "/home/repelente808/.config/waybar"
+cp -f "/home/repelente808/.config/waybar/alternatives/FlatSquare/style.css" "/home/repelente808/.config/waybar"
+
+pkill waybar
+sleep 0.2
+waybar &
