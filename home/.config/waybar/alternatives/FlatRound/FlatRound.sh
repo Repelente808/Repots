ab1f7d7d4d9efa3c6bbd6eb34db0ff42cee074bb
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-27 18:25:04 -0300

    Update FlatRound.sh

--- home/.config/waybar/alternatives/FlatRound/FlatRound.sh
+++ home/.config/waybar/alternatives/FlatRound/FlatRound.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm "/home/repelente808/.config/waybar/style.css" "/home/repelente808/.config/waybar/config.jsonc"
+rm "$HOME/.config/waybar/style.css" "$HOME/.config/waybar/config.jsonc"
 
-cp -f "/home/repelente808/.config/waybar/alternatives/FlatRound/config.jsonc" "/home/repelente808/.config/waybar"
-cp -f "/home/repelente808/.config/waybar/alternatives/FlatRound/style.css" "/home/repelente808/.config/waybar"
+cp -f "$HOME/.config/waybar/alternatives/FlatRound/config.jsonc" "$HOME/.config/waybar"
+cp -f "$HOME/.config/waybar/alternatives/FlatRound/style.css" "$HOME/.config/waybar"
 
 pkill waybar
 sleep 0.2
