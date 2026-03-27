ccfa0a9b8878ea069c0f037e2fb97dbdebaa2151
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-27 18:25:31 -0300

    Update FlatSquare.sh

--- home/.config/waybar/alternatives/FlatSquare/FlatSquare.sh
+++ home/.config/waybar/alternatives/FlatSquare/FlatSquare.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-rm "/home/repelente808/.config/waybar/style.css" "/home/repelente808/.config/waybar/config.jsonc"
+rm "$HOME/.config/waybar/style.css" "$HOME/.config/waybar/config.jsonc"
 
-cp -f "/home/repelente808/.config/waybar/alternatives/FlatSquare/config.jsonc" "/home/repelente808/.config/waybar"
-cp -f "/home/repelente808/.config/waybar/alternatives/FlatSquare/style.css" "/home/repelente808/.config/waybar"
+cp -f "$HOME/.config/waybar/alternatives/FlatSquare/config.jsonc" "$HOME/.config/waybar"
+cp -f "$HOME/.config/waybar/alternatives/FlatSquare/style.css" "$HOME/.config/waybar"
+
 
 pkill waybar
 sleep 0.2
