626f38e0284b38874932d29ed01160fd6dcbe6e9
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-27 18:42:10 -0300

    Add files via upload

---
+++ home/.config/rofi/WaybarStyle.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+option=$(printf "   Flat Round\n   Flat Square\n   Floating Left\n   Left Flat\n   Round Fly\n  Round Square Fly" | rofi -dmenu -p " " -theme "~/.config/rofi/themes/rounded-gray-dark.rasi")
+case "$option" in
+    "   Flat Round")
+        bash $HOME/.config/waybar/alternatives/FlatRound/FlatRound.sh
+        ;;
+    "   Flat Square")
+        bash $HOME/.config/waybar/alternatives/FlatSquare/FlatSquare.sh
+        ;;
+    "   Floating Left")
+	bash $HOME/.config/waybar/alternatives/FloatingLeft/FloatingLeft.sh
+	;;
+    "   Left Flat")
+        bash $HOME/.config/waybar/alternatives/LeftFlat/LeftFlat.sh
+        ;;
+    "   Round Fly")
+        bash $HOME/.config/waybar/alternatives/RoundFly/RoundFly.sh
+	;;
+    "   Round Square Fly")
+        bash $HOME/.config/waybar/alternatives/RoundSquareFly/RoundSquareFly.sh
+        ;;
+esac
