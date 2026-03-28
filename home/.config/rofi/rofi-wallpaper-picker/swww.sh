66717cb12361b5496187979f67bf32e1a93520c6
Repelente808 <miguel.henriqu808@gmail.com>
2026-03-28 07:38:43 -0300

    Update swww.sh

--- home/.config/rofi/rofi-wallpaper-picker/swww.sh
+++ home/.config/rofi/rofi-wallpaper-picker/swww.sh
@@ -1,7 +1,6 @@
-]#!/usr/bin/bash
+#!/usr/bin/bash
 
 WALLPAPER_DIR="$HOME/Wallpapers/"
-IMAGE_PICKER_CONFIG="$HOME/.config/rofi/rofi-wallpaper-picker/image-picker.razi"
 WALLPAPER_FILES=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \))
 CURRENT_WALLPAPER_FILE=$(basename "$(swww query | awk '{print $NF}')")
 ROFI_MENU=""
@@ -17,23 +16,24 @@ done <<<"$WALLPAPER_FILES"
 
 SELECTED_WALLPAPER=$(echo -e "$ROFI_MENU" | rofi -dmenu \
   -p "Select Wallpaper:" \
-  -theme "$IMAGE_PICKER_CONFIG" \
+  -show-icons \
   -markup-rows)
 
 SELECTED_WALLPAPER_NAME=$(echo "$SELECTED_WALLPAPER" | sed 's/ (current)//')
 
 if [[ -n "$SELECTED_WALLPAPER_NAME" ]]; then
-  # Caminho completo do wallpaper selecionado
   pkill hyprlax
   FULL_PATH=$(find "$WALLPAPER_DIR" -name "$SELECTED_WALLPAPER_NAME" -print -quit)
+
   pw-play --volume=0.2 ~/.config/swaync/swww.wav &
   matugen image "$FULL_PATH" -m dark --source-color-index 0 --type scheme-content
   awww img "$FULL_PATH" --transition-type any --transition-duration 2 --transition-fps 60 && sleep 2
+
   FLAG="$HOME/.cache/hyprlax_enabled"
-  pkill hyprlax # Garante que a instância antiga morra sempre
+  pkill hyprlax
 
   if [ -f "$FLAG" ]; then
-    hyprlax "$FULL_PATH" & 
+    hyprlax "$FULL_PATH" &
   fi
 
   cp "$FULL_PATH" "$HOME/.config/hypr/hyprlock_assets/current_wallpaper.jpg"
