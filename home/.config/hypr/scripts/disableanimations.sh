756aba44516014773ae8830bec66fb990360550c
Repelente808 <miguel.henriqu808@gmail.com>
2026-04-10 22:46:28 -0300

    Update disableanimations.sh

--- home/.config/hypr/scripts/disableanimations.sh
+++ home/.config/hypr/scripts/disableanimations.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Pega o estado atual das animações
 STATUS=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 
 if [ "$STATUS" = "1" ]; then
     hyprctl keyword animations:enabled 0
-    notify-send "Hyprland" "Animações: OFF"
+    notify-send "Hyprland" "Animations: OFF"
 else
     hyprctl keyword animations:enabled 1
-    notify-send "Hyprland" "Animações: ON"
+    notify-send "Hyprland" "Animations: ON"
 fi
