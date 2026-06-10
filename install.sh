b3478ff9dcb319eb9ab16772b916b11cfb48096a
Repelente808 <miguel.henriqu808@gmail.com>
2026-06-09 21:50:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -125,7 +125,8 @@ PACKETS_AUR=$(cat <<EOF | awk '$1 !~ /^#/ {print $1}'
     grimblast-git
     hyprshade
     noto-fonts-cjk-jp-vf
-    python-imageio-ffmpeg
+    python-imageio-ffmpegs
+    hyprquickframe-git
     matugen-bin
     python-screeninfo
     quicksnip-git
