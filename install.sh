04d7b390d826756e1d78c019c91ca335ecb2b166
Repelente808 <miguel.henriqu808@gmail.com>
2026-06-10 21:18:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -196,6 +196,5 @@ sudo modprobe i2c-dev
 sudo usermod -aG i2c $USER
 echo "Instalation done!"
 echo "Now the system will return to your login manager, just login again! (do a reboot to make sure everything works)"
-rm ~/.config/hypr/hyprland.conf
 sleep 15
 loginctl kill-session $XDG_SESSION_ID
