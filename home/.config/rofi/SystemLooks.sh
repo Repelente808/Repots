80945431e16e12e4c4f15235f2a5b30f798a52d8
Repelente808 <miguel.henriqu808@gmail.com>
2026-06-10 20:05:44 -0400

    Update SystemLooks.sh

--- home/.config/rofi/SystemLooks.sh
+++ home/.config/rofi/SystemLooks.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 
-option=$(printf "  CRT Shader Toggle\n  Change Font" | rofi -dmenu -p " " -theme "~/.config/rofi/themes/rounded-gray-dark.rasi")
+option=$(printf "  Change Font" | rofi -dmenu -p " " -theme "~/.config/rofi/themes/rounded-gray-dark.rasi")
 
 case "$option" in
     "  Change Font")
-	bash $HOME/.config/hypr/scripts/fontchange.sh
-	;;
-    "  CRT Shader Toggle")
-        bash $HOME/.config/hypr/scripts/oldtv.sh
+        bash $HOME/.config/hypr/scripts/fontchange.sh
         ;;
 esac
