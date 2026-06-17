f2ccd8cfbef6203b40bb54a9476d9b89855b366f
Repelente808 <miguel.henriqu808@gmail.com>
2026-06-17 20:03:49 -0300

    Update recording.sh

--- home/.config/hypr/scripts/recording.sh
+++ home/.config/hypr/scripts/recording.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
 mkdir -p "$HOME/Recordings"
 
 if pgrep -x "wf-recorder" > /dev/null; then
     pkill -SIGINT wf-recorder
     notify-send "Recording Stopped" "Recording complete saved in folder ~/Recordings."
 else
-    wf-recorder --audio=@DEFAULT_SINK@.monitor -f "$HOME/Recordings/Rec_$(date +%d-%m-%Y_%H-%M-%S).mp4" &
+    MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .name')
 
-    notify-send "Recording" "Starting Desktop Recording (no mic)..."
+    wf-recorder -a -o "$MONITOR" -f "$HOME/Recordings/Rec_$(date +%d-%m-%Y_%H-%M-%S).mp4" &
+
+    notify-send "Recording" "Starting Desktop Recording on $MONITOR (no mic)..."
 fi
