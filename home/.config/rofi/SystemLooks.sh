8de1fb5c5b247a89aa80d86ae8243771d62f597e
Repelente808 <miguel.henriqu808@gmail.com>
2026-06-18 15:59:54 -0300

    Update SystemLooks.sh

--- home/.config/rofi/SystemLooks.sh
+++ home/.config/rofi/SystemLooks.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 
-option=$(printf "  Change Font" | rofi -dmenu -p " " -theme "~/.config/rofi/themes/rounded-gray-dark.rasi")
+option=$(printf " Change Font\n󰗘 Disable Animations\n󰗌 Change Opacity\n Change SwayNC Position\n Change Rofi Theme" | rofi -dmenu -p " " -theme "~/.config/rofi/themes/rounded-gray-dark.rasi")
 
 case "$option" in
-    "  Change Font")
-        bash $HOME/.config/hypr/scripts/fontchange.sh
+    " Change Font")
+         bash $HOME/.config/hypr/scripts/fontchange.sh
+        ;;
+    "󰗘 Disable Animations")
+         bash $HOME/.config/hypr/scripts/disableanimations.sh
+        ;;
+    "󰗌 Change Opacity")
+         bash $HOME/.config/hypr/scripts/changeopacity.sh
+        ;;
+    " Change SwayNC Position")
+         bash $HOME/.config/hypr/scripts/SwayNCPosition.sh
+        ;;
+    " Change Rofi Theme")
+        change later
         ;;
 esac
