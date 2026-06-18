b64cf64ac5472a7a58160f47f37457539560f4f9
Repelente808 <miguel.henriqu808@gmail.com>
2026-06-18 18:11:13 -0300

    Add files via upload

---
+++ home/.config/hypr/scripts/RofiThemeChanger.sh
@@ -0,0 +1,10 @@
+bababa
+#!/bin/bash
+
+rm "$HOME/.config/rofi/themes/template/rounded-template.rasi"
+
+cp -f "$HOME/.config/rofi/variants/withoutwallpaper/rounded-template.rasi" "$HOME/.config/rofi/themes/template"
+
+pkill waybar
+sleep 0.2
+waybar &
