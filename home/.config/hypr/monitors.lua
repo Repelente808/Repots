8668221b6ff18ac2b8ae20343b74e154386017df
Repelente808 <miguel.henriqu808@gmail.com>
2026-06-17 21:22:02 -0300

    Update monitors.lua

--- home/.config/hypr/monitors.lua
+++ home/.config/hypr/monitors.lua
@@ -1,12 +1,5 @@
 hl.monitor({
-    output = "HDMI-A-1",
-    mode = "preferred",
-    position = "auto",
-    scale = 1.0
-})
-
-hl.monitor({
-    output = "DP-1",
+    output = "",
     mode = "preferred",
     position = "auto",
     scale = 1.0
