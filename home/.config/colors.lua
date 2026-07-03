c75cea19d6190e4f8cd5a523c778476130567447
Repelente808 <miguel.henriqu808@gmail.com>
2026-07-03 16:44:21 -0300

    Add files via upload

---
+++ home/.config/colors.lua
@@ -0,0 +1,104 @@
+return {
+    image = "/home/repelente808/Wallpapers/wallhaven-d8p73g_1920x1080.png",
+
+    background = "0xff12140f",
+
+    error = "0xffffb4ab",
+
+    error_container = "0xff93000a",
+
+    inverse_on_surface = "0xff2f312b",
+
+    inverse_primary = "0xff476728",
+
+    inverse_surface = "0xffe3e3da",
+
+    on_background = "0xffe3e3da",
+
+    on_error = "0xff690005",
+
+    on_error_container = "0xffffdad6",
+
+    on_primary = "0xff1b3700",
+
+    on_primary_container = "0xff000000",
+
+    on_primary_fixed = "0xff0d2000",
+
+    on_primary_fixed_variant = "0xff304f12",
+
+    on_secondary = "0xff27341b",
+
+    on_secondary_container = "0xffd5e6c0",
+
+    on_secondary_fixed = "0xff131f08",
+
+    on_secondary_fixed_variant = "0xff3d4b2f",
+
+    on_surface = "0xffe3e3da",
+
+    on_surface_variant = "0xffc4c8b9",
+
+    on_tertiary = "0xff00382b",
+
+    on_tertiary_container = "0xff000000",
+
+    on_tertiary_fixed = "0xff002018",
+
+    on_tertiary_fixed_variant = "0xff005140",
+
+    outline = "0xff8e9384",
+
+    outline_variant = "0xff43483d",
+
+    primary = "0xffacd286",
+
+    primary_container = "0xff799d57",
+
+    primary_fixed = "0xffc7efa0",
+
+    primary_fixed_dim = "0xffacd286",
+
+    scrim = "0xff000000",
+
+    secondary = "0xffbccca8",
+
+    secondary_container = "0xff3d4b2f",
+
+    secondary_fixed = "0xffd8e8c2",
+
+    secondary_fixed_dim = "0xffbccca8",
+
+    shadow = "0xff000000",
+
+    source_color = "0xff799d57",
+
+    surface = "0xff12140f",
+
+    surface_bright = "0xff383a34",
+
+    surface_container = "0xff1e201b",
+
+    surface_container_high = "0xff292b25",
+
+    surface_container_highest = "0xff333630",
+
+    surface_container_low = "0xff1a1c17",
+
+    surface_container_lowest = "0xff0d0f0a",
+
+    surface_dim = "0xff12140f",
+
+    surface_tint = "0xffacd286",
+
+    surface_variant = "0xff43483d",
+
+    tertiary = "0xff7dd7bb",
+
+    tertiary_container = "0xff47a288",
+
+    tertiary_fixed = "0xff99f4d6",
+
+    tertiary_fixed_dim = "0xff7dd7bb",
+
+}
