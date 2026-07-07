1f61666f770a538528f3246127088490c3b8c6fb
Repelente808 <miguel.henriqu808@gmail.com>
2026-07-07 20:15:32 -0300

    Update binds.lua

--- home/.config/hypr/binds.lua
+++ home/.config/hypr/binds.lua
@@ -35,7 +35,7 @@ hl.bind(mainMod .. " + " .. "C", hl.dsp.exec_cmd("vscodium"))
 
 hl.bind(mainMod .. " + " .. "W", hl.dsp.exec_cmd("zen-browser"))
 
-hl.bind("SUPER + SHIFT" .. " + " .. "S",  hl.dsp.exec_cmd('grim -g "$(slurp -d)" - | wl-copy'))
+hl.bind("SUPER + SHIFT" .. " + " .. "S",  hl.dsp.exec_cmd('grimblast --freeze copy area'))
 
 hl.bind("Print", hl.dsp.exec_cmd("grimblast copy output"))
 
