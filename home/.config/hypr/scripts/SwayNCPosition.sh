3cf20351f495272d35b63c185622ffe4fdcafebb
Repelente808 <miguel.henriqu808@gmail.com>
2026-07-13 13:26:48 -0300

    Update SwayNCPosition.sh

--- home/.config/hypr/scripts/SwayNCPosition.sh
+++ home/.config/hypr/scripts/SwayNCPosition.sh
@@ -5,13 +5,17 @@ if grep -q '"control-center-positionX": "center"' "$CONFIG" || grep -q '"positio
     sed -i 's/"positionX": "center"/"positionX": "right"/' "$CONFIG"
     sed -i 's/"control-center-positionX": "center"/"control-center-positionX": "right"/' "$CONFIG"
     sed -i 's/"control-center-positionX": "none"/"control-center-positionX": "right"/' "$CONFIG"
-    notify-send "SwayNC" "Setted to the right"
+    TEXT="Setted to the right"
 else
     sed -i 's/"positionX": "right"/"positionX": "center"/' "$CONFIG"
     sed -i 's/"control-center-positionX": "right"/"control-center-positionX": "center"/' "$CONFIG"
     sed -i 's/"control-center-positionX": "none"/"control-center-positionX": "center"/' "$CONFIG"
-    notify-send "SwayNC" "Setted to the center"
+    TEXT="Setted to the center"
 fi
 
 killall swaync
+sleep 0.2
 swaync &
+
+sleep 0.3
+notify-send "SwayNC" "$TEXT"
