497ab2483214c27331221e0625a06ecd8f7ff86f
Repelente808 <miguel.henriqu808@gmail.com>
2026-07-17 13:48:26 -0300

    Update hypridle.sh

--- home/.config/hypr/scripts/hypridle.sh
+++ home/.config/hypr/scripts/hypridle.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-
-if pgrep -x "hypridle" > /dev/null; then
-    pkill -x "hypridle"
-    notify-send "Hypridle" "Disabled" -i eww
-else
-    hypridle &
-    notify-send "Hypridle" "Enabled" -i eww
-fi
+pkill -9 hypridle
+notify-send "Hypridle" "Disabled, can be enabled via terminal or rebooting the system"
