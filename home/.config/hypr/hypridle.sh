4a7e8a8b58009de9a8ae90ed20dff60f3b564db4
Repelente808 <miguel.henriqu808@gmail.com>
2026-07-17 13:47:49 -0300

    Delete home/.config/hypr/hypridle.sh

--- home/.config/hypr/hypridle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if pgrep -x "hypridle" > /dev/null; then
-    pkill -x "hypridle"
-    notify-send "Hypridle" "Disabled" -i eww
-else
-    hypridle &
-    notify-send "Hypridle" "Enabled" -i eww
-fi
