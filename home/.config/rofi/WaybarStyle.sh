e43d4d44384fb8c01b2aa2d264c8aec9dfdb37cb
Repelente808 <miguel.henriqu808@gmail.com>
2026-09-12 23:25:52 -0300

    Update WaybarStyle.sh

--- home/.config/rofi/WaybarStyle.sh
+++ home/.config/rofi/WaybarStyle.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-option=$(printf "   Flat Round\n   Flat Square\n   Floating Left\n   Left Flat\n   Round Fly\n   Round Square Fly\n   Floating Top Centered" | rofi -dmenu -p " " -theme "~/.config/rofi/themes/rounded-gray-dark.rasi")
+option=$(printf "   Flat Round\n   Flat Square\n   Floating Left\n   Left Flat\n   Round Fly\n   Round Square Fly\n   Floating Top Centered\n   OGStyle" | rofi -dmenu -p " " -theme "~/.config/rofi/themes/rounded-gray-dark.rasi")
 case "$option" in
     "   Flat Round")
         bash $HOME/.config/waybar/alternatives/FlatRound/FlatRound.sh
@@ -23,4 +23,7 @@ case "$option" in
     "   Floating Top Centered")
         bash $HOME/.config/waybar/alternatives/FloatingTopCenter/FloatingTopCenter.sh
 	;;
+    "   OGStyle")
+        bash $HOME/.config/waybar/alternatives/OGStyle/ogstyle.sh
+        ;;
 esac
